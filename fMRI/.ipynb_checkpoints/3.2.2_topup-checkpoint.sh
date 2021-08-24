38a91ead68c8bd5a772b23d134737af21f4238f9
Carolyn McNabb <58777305+CarolynMcNabb@users.noreply.github.com>
2021-08-24 16:59:50 +0100

    ADD:Feat preprocessing added

--- fMRI/.ipynb_checkpoints/3.2.2_topup-checkpoint.sh
+++ fMRI/.ipynb_checkpoints/3.2.2_topup-checkpoint.sh
@@ -24,13 +24,30 @@ for sub in ${!subjects[@]}; do
 
         echo "Estimating susceptibilty induced distortion for ${i} ${ses} using FSL's topup - this will take a while"
 topup --imain=${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_b0_images.nii.gz --datain=${code_path}/acq_param.txt --config=${code_path}/b02b0.cnf --out=${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_topup_output  --iout=${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0 --fout=${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_b0_field 
-
-        echo "Performing brain extraction of hifi_b0 image for use in FEAT"
-        bet ${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0 ${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0_brain
-
+    
     done
 done
 
+echo "Creating hifi_b0_brain images for use in Feat b0 unwarping"
+
+for sub in ${!subjects[@]}; do 
+    i=${subjects[$sub]}
+    s=${i//$"sub-"/}
+    
+    cd ${derivative_path}/${i}
+    sessions=( $(ls -d ses-*))
+    for ses in ${sessions}; do
+
+        echo "Creating mean hifi_b0 image for ${i} ${ses} - will overwrite iout output from previous step"
+       fslmaths ${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0 -Tmean ${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0
+        echo "Performing brain extraction of hifi_b0 image for use in FEAT"
+       bet ${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0 ${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0_brain 
+       fslmaths ${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0_brain -ero ${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0_brain
+        echo "Opening brain extracted image (${i}_${ses}) for user approval - remove ALL non-brain!!!"
+        fsleyes ${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0 ${derivative_path}/${i}/${ses}/fmap/${i}_${ses}_hifi_b0_brain -dr 0 10000 --cmap red-yellow &
+
+    done
+done
 
 
 
